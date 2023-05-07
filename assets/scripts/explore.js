19c316c8ffb1475062837f80101e730903a8d03d
jackiepiepkorn <54698440+jackiepiepkorn@users.noreply.github.com>
2023-05-06 23:30:42 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,49 @@
 // explore.js
 
+const synth = window.speechSynthesis;
+const inputForm = document.querySelector("form");
+const inputTxt = document.querySelector("text-to-speak");
+const voiceSelect = document.querySelector('voice-select');
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  populateVoiceList();
 }
+  // Function to populate the dropdown with available voices
+  function populateVoiceList() {
+    voices = synth.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+inputForm.onsubmit = (event) => {
+  event.preventDefault();
+
+  const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute("data-name");
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+  synth.speak(utterThis);
+
+  inputTxt.blur();
+};
